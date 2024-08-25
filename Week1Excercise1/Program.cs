4b7ab6db7a9f2452c7a31859fa903fbd2c84b92c
Roxanne Taylor <rjtaylor28@amarillocollege.com>
2024-08-24 22:43:39 -0500

    Add project files.

---
+++ Week1Excercise1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1Excercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Welcomes user to class
+            Console.WriteLine("Welcome to ITSE1302");
+            //Reads line from standard input stream
+            Console.ReadLine();
+        }
+    }
+}
